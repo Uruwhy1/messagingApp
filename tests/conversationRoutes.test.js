10fdb7c22cc7e23deccdeb8a568f917cf54fed34
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-12-18 03:26:43 -0300

    divide test suite into multiple files

---
+++ tests/conversationRoutes.test.js
@@ -0,0 +1,339 @@
+const prisma = require("../prismaClient.js");
+const request = require("supertest");
+const conversationRouter = require("../routes/conversationRoutes.js");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use("/conversations", conversationRouter);
+
+describe("Conversations Routes", () => {
+  let user1, user2, user3;
+
+  beforeAll(async () => {
+    user1 = await prisma.user.create({
+      data: {
+        email: "user1@example.com",
+        password: "password1",
+        name: "User 1",
+      },
+    });
+
+    user2 = await prisma.user.create({
+      data: {
+        email: "user2@example.com",
+        password: "password2",
+        name: "User 2",
+      },
+    });
+
+    user3 = await prisma.user.create({
+      data: {
+        email: "user3@example.com",
+        password: "password3",
+        name: "User 3",
+      },
+    });
+  });
+
+  afterAll(async () => {
+    await prisma.$queryRaw`TRUNCATE TABLE "ConversationUser" RESTART IDENTITY CASCADE;`;
+    await prisma.$queryRaw`TRUNCATE TABLE "Conversation" RESTART IDENTITY CASCADE;`;
+    await prisma.$queryRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`;
+    await prisma.$disconnect();
+  });
+
+  describe("Creating Conversations", () => {
+    describe("Valid Requests", () => {
+      it("should create a new conversation and add users to it", async () => {
+        const response = await request(app)
+          .post("/conversations/create")
+          .send({
+            userIds: [user1.id, user2.id],
+            adminId: user1.id,
+          });
+
+        expect(response.status).toBe(201);
+        expect(response.body).toBe(1);
+      });
+    });
+
+    describe("Invalid Requests", () => {
+      it("should fail to create a conversation with invalid user IDs", async () => {
+        const response = await request(app)
+          .post("/conversations/create")
+          .send({
+            userIds: [user1.id, "Xd"],
+            adminId: user1.id,
+          });
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe("All user IDs must be valid numbers.");
+      });
+
+      it("should fail to create a conversation with non-existant user IDs", async () => {
+        const response = await request(app)
+          .post("/conversations/create")
+          .send({
+            userIds: [user1.id, -1],
+            adminId: user1.id,
+          });
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe("One or more users not found.");
+      });
+
+      it("should fail to create a conversation without an admin ID", async () => {
+        const response = await request(app)
+          .post("/conversations/create")
+          .send({
+            userIds: [user1.id, user2.id],
+          });
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe(
+          "Admin ID must be one of the users in the conversation."
+        );
+      });
+
+      it("should fail to create a conversation with an empty array", async () => {
+        const response = await request(app).post("/conversations/create").send({
+          userIds: [],
+          adminId: user1.id,
+        });
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe(
+          "A conversation must include at least two users."
+        );
+      });
+
+      it("should fail to create a conversation when admin is not part of users", async () => {
+        const response = await request(app)
+          .post("/conversations/create")
+          .send({
+            userIds: [user2.id, user3.id], // Admin not in userIds
+            adminId: user1.id,
+          });
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe(
+          "Admin ID must be one of the users in the conversation."
+        );
+      });
+
+      it("should fail to create a conversation without a users array", async () => {
+        const response = await request(app).post("/conversations/create").send({
+          adminId: user1.id,
+        });
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe(
+          "A conversation must include at least two users."
+        );
+      });
+    });
+  });
+
+  describe("Fetching by User", () => {
+    beforeAll(async () => {
+      await prisma.$queryRaw`TRUNCATE TABLE "ConversationUser" RESTART IDENTITY CASCADE;`;
+      await prisma.$queryRaw`TRUNCATE TABLE "Conversation" RESTART IDENTITY CASCADE;`;
+
+      await request(app)
+        .post("/conversations/create")
+        .send({
+          userIds: [user1.id, user2.id],
+          adminId: user1.id,
+        });
+    });
+
+    describe("Valid Requests", () => {
+      it("should fetch user conversations", async () => {
+        const response = await request(app).get(
+          `/conversations/user/${user1.id}`
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(1);
+      });
+
+      it("should return an empty array if the user has no conversations", async () => {
+        const response = await request(app).get(
+          `/conversations/user/${user3.id}`
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body).toHaveLength(0);
+      });
+    });
+
+    describe("Invalid Requests", () => {
+      it("should fail with a non-numeric userId", async () => {
+        const response = await request(app).get(`/conversations/user/meow`);
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe("Invalid user ID");
+      });
+
+      it("should fail when the user does not exist", async () => {
+        const response = await request(app).get(`/conversations/user/-1`);
+
+        expect(response.status).toBe(404);
+        expect(response.body.error).toBe("User not found");
+      });
+    });
+  });
+
+  describe("Fetching Specific Conversation", () => {
+    let conversation, message1, message2;
+
+    beforeAll(async () => {
+      await prisma.$queryRaw`TRUNCATE TABLE "ConversationUser" RESTART IDENTITY CASCADE;`;
+      await prisma.$queryRaw`TRUNCATE TABLE "Conversation" RESTART IDENTITY CASCADE;`;
+      await prisma.$queryRaw`TRUNCATE TABLE "Message" RESTART IDENTITY CASCADE;`;
+
+      conversation = await prisma.conversation.create({
+        data: {
+          admin: { connect: { id: user1.id } },
+          users: {
+            create: [{ userId: user1.id }, { userId: user2.id }],
+          },
+        },
+      });
+
+      message1 = await prisma.message.create({
+        data: {
+          content: "Hello",
+          authorId: user1.id,
+          conversationId: conversation.id,
+        },
+      });
+
+      message2 = await prisma.message.create({
+        data: {
+          content: "Hi there",
+          authorId: user2.id,
+          conversationId: conversation.id,
+        },
+      });
+    });
+
+    afterAll(async () => {
+      await prisma.$queryRaw`TRUNCATE TABLE "Message" RESTART IDENTITY CASCADE;`;
+      await prisma.$queryRaw`TRUNCATE TABLE "ConversationUser" RESTART IDENTITY CASCADE;`;
+      await prisma.$queryRaw`TRUNCATE TABLE "Conversation" RESTART IDENTITY CASCADE;`;
+    });
+
+    describe("Valid Requests", () => {
+      it("should fetch a conversation with its details", async () => {
+        const response = await request(app).get(
+          `/conversations/${conversation.id}`
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body.messages.length).toBe(2);
+        expect(response.body.id).toBe(conversation.id);
+      });
+
+      it("should return messages in ascending order by date", async () => {
+        const response = await request(app).get(
+          `/conversations/${conversation.id}`
+        );
+
+        expect(response.status).toBe(200);
+        expect(response.body.messages[0].content).toBe("Hello");
+        expect(response.body.messages[1].content).toBe("Hi there");
+      });
+    });
+
+    describe("Invalid Requests", () => {
+      it("should return 400 for invalid conversation ID", async () => {
+        const response = await request(app).get("/conversations/invalid");
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe("Invalid conversation ID");
+      });
+
+      it("should return 404 for non-existent conversation", async () => {
+        const response = await request(app).get("/conversations/9999");
+
+        expect(response.status).toBe(404);
+        expect(response.body.error).toBe("Conversation not found");
+      });
+    });
+  });
+
+  describe("Deleting User from Conversation", () => {
+    let conversationId;
+    beforeEach(async () => {
+      await prisma.$queryRaw`TRUNCATE TABLE "ConversationUser" RESTART IDENTITY CASCADE;`;
+      await prisma.$queryRaw`TRUNCATE TABLE "Conversation" RESTART IDENTITY CASCADE;`;
+
+      const createdConversation = await request(app)
+        .post("/conversations/create")
+        .send({
+          userIds: [user1.id, user2.id],
+          adminId: user1.id,
+        });
+
+      conversationId = createdConversation.body;
+    });
+
+    describe("Valid Requests", () => {
+      it("admin should be able to remove users", async () => {
+        const response = await request(app)
+          .delete(`/conversations/${conversationId}/users/${user2.id}`)
+          .send({
+            requestId: user1.id,
+          });
+
+        expect(response.status).toBe(200);
+      });
+
+      it("user should be able to leave", async () => {
+        const response = await request(app)
+          .delete(`/conversations/${conversationId}/users/${user2.id}`)
+          .send({
+            requestId: user2.id,
+          });
+
+        expect(response.status).toBe(200);
+      });
+    });
+
+    describe("Invalid Requests", () => {
+      it("should not be able to remove other user if not admin", async () => {
+        const response = await request(app)
+          .delete(`/conversations/${conversationId}/users/${user1.id}`)
+          .send({
+            requestId: user2.id,
+          });
+
+        expect(response.status).toBe(403);
+      });
+
+      it("invalid userId", async () => {
+        const response = await request(app)
+          .delete(`/conversations/${conversationId}/users/-1`)
+          .send({
+            requestId: user2.id,
+          });
+
+        const response2 = await request(app)
+          .delete(`/conversations/${conversationId}/users/asdsad`)
+          .send({
+            requestId: user2.id,
+          });
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe(
+          "User is not part of this conversation."
+        );
+
+        expect(response2.status).toBe(400);
+        expect(response2.body.error).toBe("IDs should be integers.");
+      });
+    });
+  });
+});
---
+++ tests/friendRoutes.test.js
@@ -0,0 +1,250 @@
+const prisma = require("../prismaClient.js");
+const request = require("supertest");
+const friendRouter = require("../routes/friendRoutes.js");
+const userRouter = require("../routes/userRoutes.js");
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+app.use("/users", userRouter);
+app.use("/friends", friendRouter);
+
+describe("Friend Routes", () => {
+  describe("Friend Requests", () => {
+    let user1, user2;
+
+    beforeAll(async () => {
+      user1 = await prisma.user.create({
+        data: {
+          email: "user1@example.com",
+          password: "password1",
+          name: "User 1",
+        },
+      });
+
+      user2 = await prisma.user.create({
+        data: {
+          email: "user2@example.com",
+          password: "password2",
+          name: "User 2",
+        },
+      });
+    });
+
+    afterAll(async () => {
+      await prisma.$queryRaw`TRUNCATE TABLE "FriendRequest" RESTART IDENTITY CASCADE;`;
+      await prisma.$queryRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`;
+      await prisma.$disconnect();
+    });
+
+    describe("Sending Friend Request", () => {
+      beforeEach(async () => {
+        await prisma.$queryRaw`TRUNCATE TABLE "FriendRequest" RESTART IDENTITY CASCADE;`;
+      });
+
+      it("should send a friend request", async () => {
+        const response = await request(app).post("/friends/send").send({
+          senderId: user1.id,
+          receiverId: user2.id,
+        });
+
+        expect(response.status).toBe(201);
+        expect(response.body.message).toBe("Friend request sent");
+      });
+
+      it("should not send duplicate friend requests", async () => {
+        await prisma.friendRequest.create({
+          data: { senderId: user1.id, receiverId: user2.id },
+        });
+
+        const response = await request(app).post("/friends/send").send({
+          senderId: user1.id,
+          receiverId: user2.id,
+        });
+
+        expect(response.status).toBe(409);
+        expect(response.body.error).toBe("Friend request already sent");
+      });
+
+      it("should not be able to send friend request to yourself", async () => {
+        const response = await request(app).post("/friends/send").send({
+          senderId: user1.id,
+          receiverId: user1.id,
+        });
+
+        expect(response.status).toBe(400);
+        expect(response.body.error).toBe(
+          "Cannot send a friend request to yourself"
+        );
+      });
+    });
+
+    describe("Accepting Friend Request", () => {
+      let requestId;
+
+      beforeEach(async () => {
+        const request = await prisma.friendRequest.create({
+          data: { senderId: user1.id, receiverId: user2.id },
+        });
+        requestId = request.id;
+      });
+
+      it("should accept a friend request", async () => {
+        const response = await request(app)
+          .post("/friends/accept")
+          .send({ requestId });
+
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe("Friend request accepted");
+      });
+
+      it("should fail to accept nonexistant request", async () => {
+        const requestId = -1;
+        const response = await request(app)
+          .post("/friends/accept")
+          .send({ requestId });
+
+        expect(response.status).toBe(404);
+      });
+    });
+
+    describe("Rejecting Friend Request", () => {
+      let requestId;
+
+      beforeEach(async () => {
+        const request = await prisma.friendRequest.create({
+          data: { senderId: user2.id, receiverId: user1.id },
+        });
+        requestId = request.id;
+      });
+
+      it("should reject a friend request", async () => {
+        const response = await request(app)
+          .post("/friends/reject")
+          .send({ requestId });
+
+        expect(response.status).toBe(200);
+        expect(response.body.message).toBe("Friend request rejected");
+      });
+
+      it("should fail to reject nonexistant request", async () => {
+        const requestId = -1;
+        const response = await request(app)
+          .post("/friends/reject")
+          .send({ requestId });
+
+        expect(response.status).toBe(404);
+      });
+    });
+
+    describe("Listing Friend Requests", () => {
+      beforeEach(async () => {
+        await prisma.$queryRaw`TRUNCATE TABLE "FriendRequest" RESTART IDENTITY CASCADE;`;
+        await prisma.friendRequest.create({
+          data: { senderId: user1.id, receiverId: user2.id },
+        });
+      });
+
+      it("should list all friend requests for a user", async () => {
+        const response = await request(app).get(
+          `/friends/listRequests/${user1.id}`
+        );
+
+        expect(response.status).toBe(200);
+        expect(Array.isArray(response.body)).toBe(true);
+        expect(response.body.length).toBe(1);
+        expect(response.body[0].senderId).toBe(user1.id);
+      });
+    });
+  });
+
+  describe("Fetching Friends", () => {
+    let userWithFriends, friend1, friend2, userWithoutFriends;
+
+    beforeAll(async () => {
+      userWithFriends = await prisma.user.create({
+        data: {
+          email: "userWithFriends@example.com",
+          password: "password123",
+          name: "User With Friends",
+          friends: {
+            create: [
+              {
+                email: "friend1@example.com",
+                password: "password1",
+                name: "Friend 1",
+              },
+              {
+                email: "friend2@example.com",
+                password: "password2",
+                name: "Friend 2",
+              },
+            ],
+          },
+        },
+        include: { friends: true },
+      });
+
+      friend1 = userWithFriends.friends[0];
+      friend2 = userWithFriends.friends[1];
+
+      userWithoutFriends = await prisma.user.create({
+        data: {
+          email: "userWithoutFriends@example.com",
+          password: "password123",
+          name: "User Without Friends",
+        },
+      });
+    });
+
+    afterAll(async () => {
+      await prisma.$queryRaw`TRUNCATE TABLE "User" RESTART IDENTITY CASCADE;`;
+      await prisma.$disconnect();
+    });
+
+    it("should fetch the friends of a user", async () => {
+      const response = await request(app).post(
+        `/users/friends/${userWithFriends.id}`
+      );
+
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.body.friends)).toBe(true);
+      expect(response.body.friends.length).toBe(2);
+
+      expect(response.body.friends).toEqual(
+        expect.arrayContaining([
+          expect.objectContaining({
+            name: friend1.name,
+          }),
+          expect.objectContaining({
+            name: friend2.name,
+          }),
+        ])
+      );
+    });
+
+    it("should return an empty array when user has no friends", async () => {
+      const response = await request(app).post(
+        `/users/friends/${userWithoutFriends.id}`
+      );
+
+      expect(response.status).toBe(200);
+      expect(Array.isArray(response.body.friends)).toBe(true);
+      expect(response.body.friends.length).toBe(0);
+    });
+
+    it("should return 404 if user is not found", async () => {
+      const response = await request(app).post("/users/friends/999");
+
+      expect(response.status).toBe(404);
+      expect(response.body.error).toBe("User not found.");
+    });
+
+    it("should return 400 if userId is invalid", async () => {
+      const response = await request(app).post("/users/friends/invalidId");
+
+      expect(response.status).toBe(400);
+      expect(response.body.error).toBe("User ID must be a valid number.");
+    });
+  });
+});
